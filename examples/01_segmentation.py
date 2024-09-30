9a25c5a29c99f6e6a373b838bb7a7c0134913791
Marek Socha <socha.marek870@gmail.com>
2024-09-30 11:32:35 +0200

    [update] Now test links properly to the data every time

--- examples/01_segmentation.py
+++ examples/01_segmentation.py
@@ -1,9 +1,13 @@
+import os
 from bronco.run import run
 
+opj = os.path.join
+dir_path = os.path.dirname(os.path.realpath(__file__))
+root = os.path.split(dir_path)[0]
 
 if __name__ == "__main__":
-    path_input = "data/input/patient1.nrrd"
-    path_supl = "data/input/patient1_lungs.nrrd"
-    path_output = "data/output"
+    path_input = opj(root, "data/input/patient1.nrrd")
+    path_supl = opj(root, "data/input/patient1_lungs.nrrd")
+    path_output = opj(root, "data/output.nrrd")
 
     run(path_input, path_output, path_supl)
