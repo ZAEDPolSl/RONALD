8dd3ec66dc78ff19f9871738a299460bed5d22b6
amrukwa <a.mrukwa00@gmail.com>
2025-01-27 07:17:46 +0000

    add lobes segmentation for carlota purposes

---
+++ bronco/segmentation/lobes_segmentation.py
@@ -0,0 +1,14 @@
+import numpy as np
+import SimpleITK as sitk
+from lungmask import mask
+
+
+def lobes_segmentation(sitk_image, config=None):
+    if config is not None:
+        model = config["model_lobes_segmentation"]
+    else:
+        model = mask.get_model('LTRCLobes')
+    lobes = mask.apply(sitk_image, model, batch_size=5)
+    sitk_lobes = sitk.GetImageFromArray(lobes)
+    sitk_lobes.CopyInformation(sitk_image)
+    return sitk_lobes
