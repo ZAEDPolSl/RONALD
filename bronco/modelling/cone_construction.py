fix numerical instability

bronco/modelling/cone_construction.py
@@ -9,6 +9,7 @@ def is_point_in_cylinder(
     semi_minor_vector1,
     semi_major_vector2,
     semi_minor_vector2,
+    eps=1e-10
 ):
     """
     Check if points are inside a cylinder with oval bases defined by semi-major and semi-minor axis vectors.
@@ -33,10 +34,9 @@ def is_point_in_cylinder(
     semi_minor_vector1 = np.array(semi_minor_vector1)
     semi_major_vector2 = np.array(semi_major_vector2)
     semi_minor_vector2 = np.array(semi_minor_vector2)
-
     # Compute cylinder axis vector and its unit vector
     axis_vector = c2 - c1
-    height = np.linalg.norm(axis_vector)
+    height = np.linalg.norm(axis_vector + eps)
     axis_unit_vector = axis_vector / height
 
     # Project points onto cylinder's axis
@@ -60,10 +60,10 @@ def is_point_in_cylinder(
     minor_projections = np.zeros(len(points))
 
     # Normalize axis vectors
-    semi_major_unit_vector1 = semi_major_vector1 / np.linalg.norm(semi_major_vector1)
-    semi_minor_unit_vector1 = semi_minor_vector1 / np.linalg.norm(semi_minor_vector1)
-    semi_major_unit_vector2 = semi_major_vector2 / np.linalg.norm(semi_major_vector2)
-    semi_minor_unit_vector2 = semi_minor_vector2 / np.linalg.norm(semi_minor_vector2)
+    semi_major_unit_vector1 = semi_major_vector1 / np.linalg.norm(semi_major_vector1 + eps)
+    semi_minor_unit_vector1 = semi_minor_vector1 / np.linalg.norm(semi_minor_vector1 + eps)
+    semi_major_unit_vector2 = semi_major_vector2 / np.linalg.norm(semi_major_vector2 + eps)
+    semi_minor_unit_vector2 = semi_minor_vector2 / np.linalg.norm(semi_minor_vector2 + eps)
 
     # Project local vectors onto semi-major and semi-minor axis vectors
     for i, choice in enumerate(base_choice):
@@ -78,10 +78,10 @@ def is_point_in_cylinder(
         minor_projections[i] = minor_projection
 
     # Calculate semi-major and semi-minor axes lengths
-    semi_major_axis1 = np.linalg.norm(semi_major_vector1)
-    semi_minor_axis1 = np.linalg.norm(semi_minor_vector1)
-    semi_major_axis2 = np.linalg.norm(semi_major_vector2)
-    semi_minor_axis2 = np.linalg.norm(semi_minor_vector2)
+    semi_major_axis1 = np.linalg.norm(semi_major_vector1 + eps)
+    semi_minor_axis1 = np.linalg.norm(semi_minor_vector1 + eps)
+    semi_major_axis2 = np.linalg.norm(semi_major_vector2 + eps)
+    semi_minor_axis2 = np.linalg.norm(semi_minor_vector2 + eps)
 
     # Check against ellipse equation
     ellipse_checks = np.zeros(len(points))

bronco/modelling/model_branch.py
@@ -5,7 +5,7 @@ from bronco.modelling.oval_construction import find_ellipse
 from bronco.modelling.cone_construction import is_point_in_cylinder
 
 
-def smooth_branch(branch, image, mode="5percent", previous_oval=None):
+def smooth_branch(branch, image, mode=None, previous_oval=None, eps=1e-10):
     """
     Smooth a branch by fitting an ellipse to each point and checking if the point is in the cylinder.
 
@@ -25,14 +25,18 @@ def smooth_branch(branch, image, mode="5percent", previous_oval=None):
         first_5_percent = math.ceil(branch.shape[0] * 0.05)
         last_5_percent = math.floor(branch.shape[0] * 0.95 - 1)
         point1 = branch[first_5_percent]
+        plane_normal1 = branch[first_5_percent+1] - point1
         point2 = branch[last_5_percent]
+        plane_normal2 = point2 - branch[last_5_percent-1]
 
     else:
         point1 = branch[0]
+        plane_normal1 = branch[1] - point1
         point2 = branch[-1]
-    plane_normal = point2 - point1
-    ellipse1 = find_ellipse(image, plane_normal, point1)
-    ellipse2 = find_ellipse(image, plane_normal, point2)
+        plane_normal2 = point2 - branch[-2]
+
+    ellipse1 = find_ellipse(image, plane_normal1, point1)
+    ellipse2 = find_ellipse(image, plane_normal2, point2)
     if previous_oval is not None:
         # check if the new ellipse is bigger than the previous one
         if np.linalg.norm(ellipse1[1]) > np.linalg.norm(
@@ -41,13 +45,13 @@ def smooth_branch(branch, image, mode="5percent", previous_oval=None):
             # normalize the new ellipse to the previous one
             new_major_axis = (
                 ellipse1[1]
-                / np.linalg.norm(ellipse1[1])
-                * np.linalg.norm(max_elipse_major)
+                / (np.linalg.norm(ellipse1[1])+eps)
+                * (np.linalg.norm(max_elipse_major)+eps)
             )
             new_minor_axis = (
                 ellipse1[2]
-                / np.linalg.norm(ellipse1[2])
-                * np.linalg.norm(max_elipse_minor)
+                / (np.linalg.norm(ellipse1[2])+eps)
+                * (np.linalg.norm(max_elipse_minor)+eps)
             )
             ellipse1 = ellipse1[0], new_major_axis, new_minor_axis
         if np.linalg.norm(ellipse2[1]) > np.linalg.norm(
@@ -55,13 +59,13 @@ def smooth_branch(branch, image, mode="5percent", previous_oval=None):
         ) or np.linalg.norm(ellipse2[2]) > np.linalg.norm(max_elipse_minor):
             new_major_axis = (
                 ellipse2[1]
-                / np.linalg.norm(ellipse2[1])
-                * np.linalg.norm(max_elipse_major)
+                / (np.linalg.norm(ellipse2[1])+eps)
+                * (np.linalg.norm(max_elipse_major)+eps)
             )
             new_minor_axis = (
                 ellipse2[2]
-                / np.linalg.norm(ellipse2[2])
-                * np.linalg.norm(max_elipse_minor)
+                / (np.linalg.norm(ellipse2[2])+eps)
+                * (np.linalg.norm(max_elipse_minor)+eps)
             )
             ellipse2 = ellipse2[0], new_major_axis, new_minor_axis
     x, y, z = np.meshgrid(

bronco/modelling/oval_construction.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.measure import label, regionprops
+import matplotlib.pyplot as plt
 
 
 def points_in_plane(points, plane_normal, plane_point):
@@ -23,19 +24,38 @@ def points_in_plane(points, plane_normal, plane_point):
     return np.isclose(projection_lengths, 0)
 
 
-def project_points_onto_plane(points, plane_normal, plane_point):
+def fit_plane(points):
+    """
+    Fit a plane to a set of 3D points using SVD.
+
+    Parameters:
+        points: numpy array of shape (N, 3) - Array of 3D points.
+
+    Returns:
+        plane_point: numpy array (3,) - A point on the plane (centroid).
+        plane_normal: numpy array (3,) - Normal vector of the plane.
+    """
+    # Compute centroid
+    plane_point = np.mean(points, axis=0)
+
+    # Perform SVD to find the normal vector
+    _, _, vh = np.linalg.svd(points - plane_point)
+    plane_normal = vh[2]  # Normal is the last row of V^T
+
+    return plane_point, plane_normal
+
+
+def project_points_onto_plane(points, eps=1e-10):
     """
     Project 3D points onto a plane defined by a normal vector and a point on the plane.
 
     Parameters:
         points: numpy array of shape (N, 3) - Array of 3D points.
-        plane_normal: numpy array of shape (3,) - Normal vector of the plane.
-        plane_point: numpy array of shape (3,) - Point on the plane.
-
     Returns:
         projected_points: numpy array of shape (N, 2) - Projected points in the plane's local coordinates.
     """
     # Compute vectors from plane point to each point
+    plane_point, plane_normal = fit_plane(points)
     vectors = points - plane_point
 
     # Project onto plane
@@ -47,7 +67,7 @@ def project_points_onto_plane(points, plane_normal, plane_point):
         perp_vector = np.cross(plane_normal, [0, 0, 1.0])
     else:
         perp_vector = np.cross(plane_normal, [0, 1.0, 0])
-    perp_vector /= np.linalg.norm(perp_vector)
+    perp_vector /= (np.linalg.norm(perp_vector) + eps)
 
     # Find another perpendicular vector (orthonormal basis)
     second_perp_vector = np.cross(plane_normal, perp_vector)
@@ -101,23 +121,15 @@ def get_mask_for_ellipse_fit(image, plane_normal, plane_point):
         local_mask: numpy array of shape (H, W) - Binary mask of points in the plane.
     """
     # get the coordinates of the points in the image
-    x, y, z = np.meshgrid(
-        np.arange(image.shape[0]),
-        np.arange(image.shape[1]),
-        np.arange(image.shape[2]),
-        indexing="ij",
-    )
-    points = np.column_stack((x.ravel(), y.ravel(), z.ravel()))
-    in_original_image = image.ravel() > 0
+    points = np.argwhere(image == 1)
     # Check which points are in the plane
     in_plane = points_in_plane(points, plane_normal, plane_point)
+    
     in_plane_projection = project_points_onto_plane(
-        points[in_plane], plane_normal, plane_point
-    )
-    for_ellipse = in_plane_projection[in_original_image[in_plane]]
-    # get the mask for the local plane
-    local_mask = create_local_mask(for_ellipse)
+        points[in_plane])
 
+    # get the mask for the local plane
+    local_mask = create_local_mask(in_plane_projection)
     return local_mask
 
 
@@ -156,6 +168,7 @@ def transform_ellipse_to_3d(
     semi_major_axis,
     semi_minor_axis,
     rotation_angle,
+    eps=1e-10
 ):
     """
     Transform 2D ellipse parameters to 3D space.
@@ -183,7 +196,7 @@ def transform_ellipse_to_3d(
         perp_vector = np.cross(plane_normal, [0.0, 0.0, 1.0])
     else:
         perp_vector = np.cross(plane_normal, [0.0, 1.0, 0.0])
-    perp_vector /= np.linalg.norm(perp_vector)
+    perp_vector /= (np.linalg.norm(perp_vector) + eps)
 
     # Find another perpendicular vector (orthonormal basis)
     second_perp_vector = np.cross(plane_normal, perp_vector)
@@ -217,7 +230,6 @@ def transform_ellipse_to_3d(
         minor_axis_vector_2d[0] * perp_vector
         + minor_axis_vector_2d[1] * second_perp_vector
     )
-
     return ellipse_center_3d, major_axis_vector_3d, minor_axis_vector_3d
 
 

bronco/modelling/smooth_tree.py
@@ -1,6 +1,7 @@
 import numpy as np
 import SimpleITK as sitk
 from skimage.morphology import skeletonize_3d
+from tqdm import tqdm
 from bronco.external.sknw import build_sknw
 from bronco.modelling.model_branch import smooth_branch
 
@@ -49,7 +50,7 @@ def model_tree(bronco_mask):
     tree_mask = np.zeros_like(bronco_mask_arr)
     node_order = get_node_order(airways_graph)
     max_oval = {node: None for node in node_order}
-    for node in node_order:
+    for node in tqdm(node_order):
         for neighbor in airways_graph.neighbors(node):
             # if neighbor is before node in node_order, then it was already processed
             if node_order.index(neighbor) < node_order.index(node):