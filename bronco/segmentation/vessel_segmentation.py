27fbc7ba97e7dc00e75e898f1051a5915adf6a6a
amrukwa <a.mrukwa00@gmail.com>
2025-05-12 11:48:08 +0200

    add vessels cleanup

--- bronco/segmentation/vessel_segmentation.py
+++ bronco/segmentation/vessel_segmentation.py
@@ -1,6 +1,8 @@
 import itk
 import numpy as np
 import SimpleITK as sitk
+from skimage.measure import regionprops
+
 
 from bronco.external.sitk2itk import (
     ConvertItkImageToSimpleItkImage,
@@ -8,7 +10,7 @@ from bronco.external.sitk2itk import (
 )
 
 
-def vessel_segmentation(sitk_image, sitk_lungs):
+def vesselness_filter(sitk_image, sitk_lungs):
     sigma = 1.0
     alpha1 = 0.5
     alpha2 = 2.0
@@ -69,3 +71,39 @@ def vessel_segmentation(sitk_image, sitk_lungs):
     direction = float_sitk.GetDirection()
     sitk_vessels = ConvertItkImageToSimpleItkImage(output_image, 8, direction)
     return sitk_vessels
+
+
+def vessel_segmentation(
+    sitk_image, sitk_lungs, sitk_lobes=None, sitk_mediastinum=None, binary=True
+):
+    sitk_vessels = vesselness_filter(sitk_image, sitk_lungs)
+    if sitk_mediastinum is None:
+        from bronco.segmentation import mediastinum_segmentation
+
+        sitk_mediastinum = mediastinum_segmentation(sitk_lungs)
+    if sitk_lobes is None:
+        from bronco.segmentation import lobe_segmentation
+
+        sitk_lobes = lobe_segmentation(sitk_image)
+
+    lungs = sitk.GetArrayFromImage(sitk_lungs)
+    vessels = sitk.GetArrayFromImage(sitk_vessels)
+    mediastinum = sitk.GetArrayFromImage(sitk_mediastinum)
+    lobes = sitk.GetArrayFromImage(sitk_lobes)
+
+    blood_system = np.logical_or(mediastinum, vessels).astype(int)
+    blood_regions = regionprops(blood_system)
+    sorted_regions = sorted(blood_regions, key=lambda x: x.area)
+    first_region_mask = np.zeros_like(blood_system, dtype=int)
+    first_region_mask[tuple(sorted_regions[0].coords.T)] = 1
+    first_region_mask[mediastinum > 1] = 0
+    first_region_mask[first_region_mask > 0] = lobes[first_region_mask > 0]
+    first_region_mask[lungs == 0] = 0
+
+    if binary:
+        first_region_mask[first_region_mask > 0] = 1
+
+    vessels_connected = sitk.GetImageFromArray(first_region_mask)
+    vessels_connected.CopyInformation(sitk_vessels)
+
+    return vessels_connected
