c668b5e98c3a1b4e0ec72d65558d337e884738ec
amrukwa <a.mrukwa00@gmail.com>
2025-11-10 10:49:50 +0100

    update example

--- examples/03_vessels.py
+++ examples/03_vessels.py
@@ -10,17 +10,13 @@ root = os.path.split(dir_path)[0]
 if __name__ == "__main__":
     path_data = opj(root, "data/patient1.nrrd")
     path_lungs = opj(root, "data/lungs1.nrrd")
-    path_lobes = opj(root, "data/lobes1.nrrd")
-    path_mediastinum = opj(root, "data/mediastinum1.nrrd")
 
     path_output = opj(root, "data/vessels1.nrrd")
 
     ii = ImageInstance()
     sitk_image = ii.read(path_data)
     sitk_lungs = ImageInstance().read(path_lungs)
-    sitk_lobes = ImageInstance().read(path_lobes)
-    sitk_mediastinum = ImageInstance().read(path_mediastinum)
-   
-    sitk_vessels = vessel_segmentation(sitk_image, sitk_lungs, sitk_lobes, sitk_mediastinum)
+
+    sitk_vessels = vessel_segmentation(sitk_image, sitk_lungs)
     ii.write(sitk_vessels, path_output)
     print("Done!")
